21ae0ece7f85d7d30437de0687ccf476247bb52b
JamakaDev <jamakaDev@gmail.com>
2022-06-26 04:24:12 -0400

    implemented search func & updated html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
 
 def index(request):
+    entries = util.list_entries()
+    if request.method == 'POST':
+        query = request.POST['q'].upper()
+        if query:
+            for que in entries:
+                if query in que.upper():
+                    return render(request, "encyclopedia/entry.html", {
+                        'title': que,
+                        'description': util.get_entry(que).split()[3:]
+                    })
     return render(request, "encyclopedia/index.html", {
-        "entries": sorted(util.list_entries())
+        "entries": entries
     })
 
 def entry(request, title):
+    entries = util.list_entries()
+    if request.method == 'POST':
+        query = request.POST['q'].upper()
+        if query:
+            for que in entries:
+                if query in que.upper():
+                    return render(request, "encyclopedia/entry.html", {
+                        'title': que,
+                        'description': util.get_entry(que).split()[3:]
+                    }) 
     tmp, title = title, util.get_entry(title)
     if not title:
         return render(request, "encyclopedia/error.html", {'title': tmp})
     return render(request, "encyclopedia/entry.html", {
         'title': tmp,
-        'description': title.split()[2:] 
+        'description': title.split()[3:] 
     })
 
