818119286cdf7b94b49d3788fc6a2f5e9c3a7fa5
JamakaDev <jamakaDev@gmail.com>
2022-06-26 18:45:15 -0400

    finished search functionality along with added some CSS

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,30 +5,23 @@ from . import util
 
 def index(request):
     entries = util.list_entries()
-    if request.method == 'POST':
-        query = request.POST['q'].upper()
-        if query:
-            for que in entries:
-                if query in que.upper():
-                    return render(request, "encyclopedia/entry.html", {
-                        'title': que,
-                        'description': util.get_entry(que).split()[3:]
-                    })
     return render(request, "encyclopedia/index.html", {
         "entries": entries
-    })
+    });
 
 def entry(request, title):
-    entries = util.list_entries()
     if request.method == 'POST':
+        query_list, entries =  [], util.list_entries()
         query = request.POST['q'].upper()
         if query:
             for que in entries:
-                if query in que.upper():
-                    return render(request, "encyclopedia/entry.html", {
-                        'title': que,
-                        'description': util.get_entry(que).split()[3:]
-                    }) 
+                info = util.get_entry(que)
+                if query in info.upper():
+                    query_list.append(info.split()[2])
+            return render(request, "encyclopedia/entry.html", {
+                'title': 'Query Search',
+                'description': query_list,
+            });
     tmp, title = title, util.get_entry(title)
     if not title:
         return render(request, "encyclopedia/error.html", {'title': tmp})
