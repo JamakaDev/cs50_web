17a7d87142b217e80f7321a4d5c0934070f51408
jamakadev <jamakadev@gmail.com>
2022-07-13 09:33:40 -0400

    created the models for the database

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -6,17 +6,43 @@ class User(AbstractUser):
     pass
 
 
-class Listings:
-    pass
+class Bid(models.Model):
+    starting_bid = models.DecimalField(max_digits=15, decimal_places=2, default=1.00)
+    previous_bid = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
+    current_bid = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
+    total_bids = models.PositiveSmallIntegerField(name='total_bids')
+    duration = models.DateTimeField(name='end_date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'${self.current_bid}'
 
 
-class Bids:
-    pass
+class Comment(models.Model):
+    text = models.TextField(name='comment_text')
+    likes = models.PositiveIntegerField(name='comment_likes')
+    posted = models.DateTimeField(name='time_posted')
+    thread = models.BooleanField(name='is_thread', default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.text
 
 
-class Comments:
-    pass
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    image = models.URLField(name='image_url')
+    category = models.CharField(max_length=24)
+    condition = models.CharField(max_length=5)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
 
 
-class Watchlist:
-    pass
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
